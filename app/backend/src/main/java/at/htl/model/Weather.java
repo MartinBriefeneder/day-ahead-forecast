70763c8ea6d121440d2a8b5b230ee07856d041c1
MartinBriefeneder <m.briefeneder@htblaleonding.onmicrosoft.com>
2026-07-14 10:13:04 +0200

    weather model

--- app/backend/src/main/java/at/htl/model/Weather.java
+++ app/backend/src/main/java/at/htl/model/Weather.java
@@ -1,4 +1,4 @@
-package model;
+package at.htl.model;
 
 public record Weather(float mean_total_precipitation_rate, float snow_cover, float two_m_dewpoint, float skin_temperature, float surface_pressure, float ten_m_wind_speed, float ten_m_wind_direction, float ten_m_relative_humidity){
 
